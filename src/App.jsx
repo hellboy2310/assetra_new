be30fb590f9f97c6e5c492885bc52e71fa47963a
bhavesh2310 <bhavesh.purohit@o2h.com>
2025-08-16 17:53:02 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center gap-6">
+      <h1>
+        Assetra new
+      </h1>
+    </div>
+  );
+}
+
+export default App
