bfcf5083b0a7842061c1f5ff4bda72fa2fb87993
bhavesh2310 <bhavesh.purohit@o2h.com>
2025-08-20 22:13:27 +0530

    fix: removed code for best practices

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,87 +1,10 @@
-import ServicesGrid from '../common/ServicesGrid';
+
 
 
 const Home = () => {
     return (
         <>
-            <div className="relative group w-full h-[700px]">
-                {/* Content wrapper (headings + buttons together) */}
-                <div className="absolute top-[198px] left-0 right-0 flex flex-col items-start md:items-center text-left md:text-center px-4">
-                    <h1 className="text-h1 text-white mb-4">
-                        Your Global Workforce Experts
-                    </h1>
-                    <h2 className="text-h3 text-white font-roboto mb-6">
-                        Unlock global talent with ASSETra’s tailored workforce solutions.
-                    </h2>
-
-                    {/* Buttons */}
-                    <div className="flex flex-row  justify-center md:flex-row items-start md:items-center text-left md:text-center space-x-3 md:space-y-0 md:space-x-5">
-                        <button className="w-[142px] h-[44px] border border-white text-white rounded-lg">
-                            Get Started
-                        </button>
-                        <button className="w-[142px] h-[44px] border border-white text-white rounded-lg">
-                            Learn More
-                        </button>
-                    </div>
-
-                </div>
-
-
-
-                {/* Background */}
-                <img
-                    className="w-full h-full object-cover"
-                    src="/assets/banner_assetra.png"
-                />
-
-                <div className="absolute inset-0 bg-primaryColor/40 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
-            </div>
-
-            <div className="w-full md:h-[250px]  flex flex-col md:flex-row items-center justify-center z-10 bg-white space-y-12 md:space-y-0 md:space-x-[5%] rounded-xl shadow-lg p-4 md:p-0">
-                <div className="flex flex-col items-center space-y-2">
-                    <h1 className="text-h4 text-primaryColor">85+</h1>
-                    <p>Clients Globally</p>
-                </div>
-                <div className="flex flex-col items-center space-y-2">
-                    <h1 className="text-h4 text-primaryColor">12+</h1>
-                    <p>Years of Experience</p>
-                </div>
-                <div className="flex flex-col items-center space-y-2">
-                    <h1 className="text-h4 text-primaryColor">50+</h1>
-                    <p>Talented Professionals</p>
-                </div>
-                <div className="flex flex-col items-center space-y-2">
-                    <h1 className="text-h4 text-primaryColor">35%</h1>
-                    <p>Referral Business</p>
-                </div>
-                <div className="flex flex-col items-center space-y-2">
-                    <h1 className="text-h4 text-primaryColor">10%</h1>
-                    <p>Paperless Commitment</p>
-                </div>
-            </div>
-
-
-            <div className="h-650 w-full flex flex-col items-center justify-center md:flex-row md:items-start md:justify-center space-x-0 p-[10px] md:space-x-20 my-10 md:my-40">
-                <div>
-                    <img className="w-[400px] h-[350px]" src="/assets/aboutus.png" alt="About Us" />
-                </div>
-                <div className="space-y-5 p-[20px] md:p-[0px] mt-10 md:mt-0 text-center md:text-left">
-                    <h1 className="text-h3 text-primaryColor">ABOUT US</h1>
-                    <div className="space-y-4 max-w-[620px] pr-0 md:pr-10">
-                        <p className="font-roboto font-light text-18">
-                            Welcome to ASSETra, a leading business consulting firm operating globally.
-                            Our expert team helps businesses achieve sustainable growth with tailored
-                            strategies.
-                        </p>
-                        <p className="font-roboto font-light text-18">
-                            We specialize in innovation & expertise, ensuring high-impact results. At
-                            ASSETra, we drive businesses forward with smart solutions.
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <ServicesGrid/>
+            
 
 
 
